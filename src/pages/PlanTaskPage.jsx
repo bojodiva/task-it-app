cf7c08b7038e3a65a8114d642346363274cb3da7
bojodiva <ojoabisola387@gmail.com>
2024-12-11 12:14:50 +0100

    documentation

--- src/pages/PlanTaskPage.jsx
+++ src/pages/PlanTaskPage.jsx
@@ -26,18 +26,16 @@ export default function PlanTaskPage(){
         }
     }
 
-    // console.log(allTasks)
-    if(!allTasks){
-        return <p>No Task has been planned yet.</p>
-    }
 
     return(
         <>
         <div className="min-h-screen bg-accent flex flex-col">
+        {/* header implementation */}
         <div className="flex justify-evenly items-center gap-1 p-3 bg-main text-accent text-center">
             <p className="text-xl">Now let us get into planning your task!</p>
             <button onClick={handleTrackTaskClick} className="rounded p-3 bg-complementary px-10 text-sm md:text-lg">Track My Tasks</button>
         </div>
+
         <div className="p-10 flex flex-col justify-center">
                 <table className="w-full table-fixed border-collapse border-spacing-2 border border-slate-300">
                     <thead>
@@ -48,9 +46,11 @@ export default function PlanTaskPage(){
                             <th className="border border-slate-300 p-2 w-1/3"></th>
                         </tr>
                     </thead>
+                    
+            {/* checking to see if there are any task planned, if true mapping through the array of tasks and if not returning "no planned task" to the user */}
                     {allTasks && allTasks.length > 0 ? (
-            allTasks.map((task) => (
-                    <tbody key={task.name} className="text-center">
+                        allTasks.map((task) => (
+                       <tbody key={task.name} className="text-center">
                         <tr>
                             <td className="border border-slate-300 p-2">{task.name}</td>
                             <td className="hidden md:table-cell border border-slate-300 p-2 text-left break-words">{task.description}</td>
@@ -67,6 +67,8 @@ export default function PlanTaskPage(){
             )}
             </table>
         </div>
+
+        {/* form implementation */}
         <div className="flex justify-center p-5">
            <form onSubmit={handleSaveClick} className="flex flex-col p-5 gap-3">
              <label className="flex justify-start">Task Name</label>
